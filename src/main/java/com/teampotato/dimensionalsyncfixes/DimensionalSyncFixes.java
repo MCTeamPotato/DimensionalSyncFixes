3a344715381ce128c9e42c4aa5e5cc96bfd143ee
Kasualix <670067575@qq.com>
2023-12-16 16:09:19 +0800

    fabric

---
+++ src/main/java/com/teampotato/dimensionalsyncfixes/DimensionalSyncFixes.java
@@ -0,0 +1,8 @@
+package com.teampotato.dimensionalsyncfixes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DimensionalSyncFixes implements ModInitializer {
+	@Override
+	public void onInitialize() {}
+}
