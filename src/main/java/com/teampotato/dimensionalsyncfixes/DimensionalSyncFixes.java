65269a39bdb3849d0c41c85cc280ddc73bba2914
Kasualix <670067575@qq.com>
2024-02-29 14:42:47 +0800

    0.0.2

--- src/main/java/com/teampotato/dimensionalsyncfixes/DimensionalSyncFixes.java
+++ src/main/java/com/teampotato/dimensionalsyncfixes/DimensionalSyncFixes.java
@@ -1,8 +1,25 @@
 package com.teampotato.dimensionalsyncfixes;
 
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityWorldChangeEvents;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+
+import java.util.Map;
 
 public class DimensionalSyncFixes implements ModInitializer {
 	@Override
-	public void onInitialize() {}
+	public void onInitialize() {
+		ServerEntityWorldChangeEvents.AFTER_PLAYER_CHANGE_WORLD.register((player, origin, destination) -> {
+			player.addExperience(0);
+			final Map<StatusEffect, StatusEffectInstance> activeStatusEffects = player.getActiveStatusEffects();
+			synchronized (activeStatusEffects) {
+				new Object2ObjectOpenHashMap<>(activeStatusEffects).forEach((statusEffect, statusEffectInstance) -> {
+					player.removeStatusEffect(statusEffect);
+					player.addStatusEffect(statusEffectInstance);
+				});
+			}
+		});
+	}
 }
